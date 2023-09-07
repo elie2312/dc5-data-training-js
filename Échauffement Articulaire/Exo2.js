e0587485d77adc528ad854dc61f88f18d5d859c9
elie2312 <elie.bannes12@gmail.com>
2023-09-07 09:52:39 +0200

    exo2

---
+++ Échauffement Articulaire/Exo2.js
@@ -0,0 +1,14 @@
+var anneeATester = 2023;
+
+
+function Bissextile(annee) {
+    if ((annee % 4 === 0 && annee % 100 !== 0) || (annee % 400 === 0)) {
+      return "L'année est bissextile";
+    } else {
+      return "L'année n'est pas bissextile";
+    }
+  }
+  
+  var resultat = Bissextile(anneeATester);
+  console.log(resultat);
+  
